465dc499650d69379b8ad5d8a7a5fc409d57a913
Victoria Nemenova <vikalangor@gmail.com>
2021-10-02 17:33:19 +0300

    second block

---
+++ movies-explorer/src/components/Main/Promo/Promo.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Promo(props) {
+
+    return (
+        <main className="content">
+            
+
+            
+        </main>
+    )
+
+}
+export default Promo;
