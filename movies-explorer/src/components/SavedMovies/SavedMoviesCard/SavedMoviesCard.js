84937374b723a5aea745265266062a0eb4aff16f
Victoria Nemenova <vikalangor@gmail.com>
2021-10-10 22:02:03 +0300

    comments fixed

--- movies-explorer/src/components/SavedMovies/SavedMoviesCard/SavedMoviesCard.js
+++ movies-explorer/src/components/SavedMovies/SavedMoviesCard/SavedMoviesCard.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import cardImg from '../../../images/cardImage.png'
+import movieList from '../../../utils/moviesList';
 
 
 function MoviesCardSaved(props) {
 
     return (
-        <li className="card">
-            <div className="card-btn">
-                <button className="card__delete-btn"></button>
-            </div>
-            <img src={cardImg} alt="" className="card__image" />
-            <div className="card__content">
-                <h4 className="card__title">33 слова о дизайне</h4>
-                <span className="card__duration">1ч 17м</span>
-            </div>
-        </li>
+        <>
+            {movieList.map((i, index) => (
+                <li key={index} className="card">
+                    <div className="card-btn">
+                        <button className="card__delete-btn"></button>
+                    </div>
+                    <img src={cardImg} alt={i.img.alt} className="card__image" />
+                    <div className="card__content">
+                        <h4 className="card__title">{i.title}</h4>
+                        <span className="card__duration">{i.duration}</span>
+                    </div>
+                </li>
+            ))}
+        </>
     )
 
 }
